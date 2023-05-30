c0ea3356d9844294634071a11dfdf928ffad344d
Nishant Patil <nishant.apatil3@gmail.com>
2023-05-30 21:37:21 +0000

    add env file

--- manual-backup.sh
+++ manual-backup.sh
@@ -10,4 +10,3 @@ echo "Backing up ${DB_DATABASE_NAME} to ${UPLOAD_LOCATION}/db_dumps"
 mkdir -p ${UPLOAD_LOCATION}/db_dumps
 
 docker exec -t immich_postgres pg_dumpall -c -U postgres | gzip > "${UPLOAD_LOCATION}/db_dumps/dump.sql.gz"
-
