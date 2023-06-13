d3f5b43918d458efaa1c209d890918b2860d20df
Nishant Patil <nishant.apatil3@gmail.com>
2023-06-12 20:23:50 -0700

    Create cli-upload.sh

---
+++ immich/scripts/cli-upload.sh
@@ -0,0 +1,6 @@
+
+API_KEY="SN7rnTmHJoUURdu7hq9fN7Fx5MKbbCIUaxYg2XUM"
+IMMICH_SERVER="192.168.4.69:8080"
+
+alias immich='docker run -it --rm -v "$(pwd):/import" ghcr.io/immich-app/immich-cli:latest'
+immich upload --key $API_KEY --server http://$IMMICH_SERVER/api --recursive
