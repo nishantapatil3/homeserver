3d5be6934c42560b2a7e690fbd8c599b5b13592d
nishantapatil3 <nishant.apatil3@gmail.com>
2023-12-30 20:49:45 -0800

    update immich

--- immich/scripts/cli-upload.sh
+++ immich/scripts/cli-upload.sh
@@ -6,8 +6,8 @@ find . -name "._IMG*" -print0 | xargs -0 rm
 find . -name ".DS_Store" -print0 | xargs -0 rm
 ######
 
-API_KEY="SN7rnTmHJoUURdu7hq9fN7Fx5MKbbCIUaxYg2XUM"
-IMMICH_SERVER="192.168.4.69:8080"
+API_KEY="OMpIGgVBqjFRsD2eR2AMkwlrdH7gF7J6Fl9XDOfjw"
+IMMICH_SERVER="127.0.0.1:3001"
 
 alias immich='docker run -it --rm -v "$(pwd):/import" ghcr.io/immich-app/immich-cli:latest'
 immich upload --key $API_KEY --server http://$IMMICH_SERVER/api --recursive
